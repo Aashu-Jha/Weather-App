73cee4da576f03ab3996947f74a13bf3b60b3706
Aashu_Jha <aashish2000kumar@gmail.com>
2021-01-01 12:33:41 +0530

    refactor: added networking.dart for api calls

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,10 @@
+import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'location_screen.dart';
 import 'package:clima/utilities/constants.dart';
+import 'package:clima/services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -16,39 +18,34 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = new Location();
     await location.getCurrentLocation();
     latitude = location.latitude;
     longitude = location.longitude;
-    getData();
+
+    NetworkHelper networkHelper = NetworkHelper('http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
+    
+    Navigator.push(context, MaterialPageRoute(builder: (context) => LocationScreen()));
   }
-
-  void getData() async{
-    http.Response response = await http.get('http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
-    if(response.statusCode == 200){
-      String data = response.body;
-
-      var temp = jsonDecode(data)['main']['temp'];
-      var id = jsonDecode(data)['weather'][0]['id'];
-      var name = jsonDecode(data)['name'];
-
-      print(temp);
-      print(id);
-      print(name);
-
-    }
-    else
-      print(response.statusCode);
-  }
-
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Center(
+      child: SpinKitWave(
+        color: Colors.white,
+        size: 50.0,
+      ),
     );
   }
 }
+
+
+// var temp = jsonDecode(data)['main']['temp'];
+// var id = jsonDecode(data)['weather'][0]['id'];
+// var name = jsonDecode(data)['name'];
